b4fd0fb67ab93ff32aaf9983753391cbd7eb7f5b
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2014-01-16 01:21:09 +0900

    coming soon

---
+++ js/adsense-data.js
@@ -0,0 +1,24 @@
+window.adsense_data = {
+	mobile_ad: '',
+	desktop_ad: ''
+}
+
+window.adsense_data.mobile_ad = 
+	'<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>\n'
+	+'<ins class="adsbygoogle"'
+	+'style="display:inline-block;width:468px;height:60px"'
+	+'data-ad-client="ca-pub-9672338973208782"'
+	+'data-ad-slot="6679159363"></ins>'
+	+'<script>'
+	+'(adsbygoogle = window.adsbygoogle || []).push({});'
+	+'</script>';
+
+window.adsense_data.desktop_ad =
+	'<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>'
+	+'<ins class="adsbygoogle"'
+	+'	style="display:inline-block;width:468px;height:60px"'
+	+'	data-ad-client="ca-pub-9672338973208782"'
+	+'	data-ad-slot="6679159363"></ins>'
+	+'<script>'
+	+'(adsbygoogle = window.adsbygoogle || []).push({});'
+	+'</script>';
--- posts/index.md
+++
@@ -1,2 +0,0 @@
-# test
-We’ve collected some useful patterns and snippets for Harp in the Harp recipes section. If you’re looking how to make a sitemap, take a look at this sitemap recipe.
