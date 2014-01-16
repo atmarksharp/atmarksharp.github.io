e288519003b77c8e67d6df9752561c86cab0d94d
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2014-01-17 00:39:56 +0900

    pre-tab.js fixed

--- js/pre-tab.js
+++ js/pre-tab.js
@@ -8,12 +8,12 @@ window.Pretab.fix = function(){
 	$('pre code').each(function(){
 		var $this = $(this);
 		var code = ($this.html());
-		
+
 		if(code){
 			var lines = code.split('\n');
 			var ret = [];
 			$.each(lines, function(i, line){
-				var s = line.replace(/^([ ]+)/g, '$1$1$1$1$1$1$1$1');
+				var s = line.replace(/^([ ]+)/g, '$1$1$1$1');
 				ret.push(s);
 			});
 
