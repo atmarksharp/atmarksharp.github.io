607970ea1db0de773a0dbdfbce8832845c6d2448
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2014-01-16 16:08:32 +0900

    highlight bug fixed

--- js/pre-tab.js
+++ js/pre-tab.js
@@ -2,14 +2,22 @@
 * Replace tabs to spaces
 */
 
-Zepto(function($){
-	var code = ($('pre code').html());
-	var lines = code.split('\n');
-	var ret = [];
-	$.each(lines, function(i, line){
-		var s = line.replace(/^([ ]+)/g, '$1$1$1$1$1$1$1$1');
-		ret.push(s);
-	});
+window.Pretab = {};
 
-	$('pre code').html(ret.join('\n'));
-});
+window.Pretab.fix = function(){
+	$('pre code').each(function(){
+		var $this = $(this);
+		var code = ($this.html());
+		
+		if(code){
+			var lines = code.split('\n');
+			var ret = [];
+			$.each(lines, function(i, line){
+				var s = line.replace(/^([ ]+)/g, '$1$1$1$1$1$1$1$1');
+				ret.push(s);
+			});
+
+			$this.html(ret.join('\n'));
+		}
+	});
+};
